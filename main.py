60d6d0f4cabbe3b94073f69a5874b7a9cc2b087a
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-02 05:48:49 -0500

    search all articles and issues for string

--- main.py
+++ main.py
@@ -3,33 +3,37 @@ import urllib3
 from bs4 import BeautifulSoup
 http = urllib3.PoolManager()
 
-#print console output to text file
+# print console output to text file
 sys.stdout = open('output.txt', 'wt')
 
 overflow_urls = []
 s1 = "buffer overflow"
-s2 = "Overflow"
-s3 = "overflow"
 issue = 1
 not_found = "404 Not Found"
 
-while issue < 71:
+while issue < 70:
+    # search within issue's articles
+    for x in range(2, 16):
+        # Get page data
+        purl = f"http://phrack.org/issues/{issue}/{x}.html"
+        page = http.request('GET', purl)
+        soup = BeautifulSoup(page.data, 'html.parser').contents
+        # converts list to string for searching
+        page1 = ''.join(map(str, soup))
+        # perform search and continue to next article
+        if page1.find(s1) != -1:
+            overflow_urls.append(purl)
+            continue
+        else:
+            continue
     # Get page data
     purl = f"http://phrack.org/issues/{issue}/1.html"
     page = http.request('GET', purl)
     soup = BeautifulSoup(page.data, 'html.parser').contents
     # converts list to string for searching
     page1 = ''.join(map(str, soup))
-    # search string
+    # search string and go to next issue
     if page1.find(s1) != -1:
-        overflow_urls.append(purl)
-        issue += 1
-        continue
-    elif page1.find(s2) != -1:
-        overflow_urls.append(purl)
-        issue += 1
-        continue
-    elif page1.find(s3) != -1:
         overflow_urls.append(purl)
         issue += 1
         continue
@@ -37,5 +41,5 @@ while issue < 71:
         issue += 1
         continue
 
-# print list of url's with searched text
+# print list of links with searched text
 print(overflow_urls)
